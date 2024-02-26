a6001c6e71f18d6a9659065ea9642a033bbcc072
laxmankori <101863822+laxmankori@users.noreply.github.com>
2024-02-26 22:48:35 +0530

    make responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,49 @@
 import React from 'react'
+import { useState } from 'react';
 import './Navbar.scss';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = event => {
+    event.currentTarget.classList.toggle('fa-xmark');
+    let a = document.querySelector(".hambuger_baner");
+    let b = document.querySelector(".navList");
+    let c = document.querySelector(".navList ul");
+    if (toggle === false) {
+      a.style.display = 'block';
+      b.style.display = 'block';
+      c.style.display = 'block';
+    }else{
+      a.style.display = 'none';
+      b.style.display = 'none';
+      c.style.display = 'none';
+    }
+  };
+
+
+
   return (
     <nav>
+      <div className='hambuger_baner' ></div>
+       
+    
         <div className="navbar">
             <div className="search">
               <img src="./images/search icon.png" alt="" />
               <input type="text"/>
             </div>
-            <ul>
+            <div className="navList">
+              <ul>
               <li>Categories</li>
               <li>Website Builders</li>
               <li className='padding-left'>Today's deals</li>
             </ul>
-        </div>
+            </div>
+<div className="hambuger" onClick={()=> setToggle(!toggle)}>
+            <i class="fa-solid fa-bars" onClick={handleClick}></i>
+          </div>
+          </div>
     </nav>
   )
 }
